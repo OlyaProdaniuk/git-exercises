cc67df13987426d3074eb8192fc52b29e82aa927
OlyaProdaniuk <digitaliz29@gmail.com>
2025-02-06 13:07:55 +0800

    smth
    stupid little addition

--- lol.js
+++ lol.js
@@ -1,1 +1,24 @@
-console.log("lol4");
+console.log("lol5");
+console.log("hello1");
+
+// For making sure your feature branch is up to date with the main branch (production branch)
+// git branch - to see your still on your feature branch
+// git rebase origin/main - to update your feature branch with the main branch
+// resolve conflicts if any in vscode
+// remember to stage the changes after resolving conflicts
+// and hit the blue continue button in the source control tab
+// if all good -> git push --force-with-lease
+
+// For making sure your local feature branch is up to date after a colleague has pushed changes to it
+// git pull --rebase
+// resolve conflicts if any in vscode
+// Remember to stage the changes after resolving conflicts
+// and hit the blue continue button in the source control tab
+// if all good -> git push --force-with-lease
+
+// For rewriting the commit message or the commit content
+// git add . (like always)
+// git commit --amend
+// --> opens up a editor where you can change the commit message
+// --> save and close the editor
+// git push --force-with-lease
